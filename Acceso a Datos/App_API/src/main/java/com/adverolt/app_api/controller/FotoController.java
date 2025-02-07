3227c28543346a065eb1d5bc5bca6ed01da87c2a
usuario <davpalmol@alu.edu.gva.es>
2025-02-07 16:47:20 +0100

    Probant fotos

---
+++ Acceso a Datos/App_API/src/main/java/com/adverolt/app_api/controller/FotoController.java
@@ -0,0 +1,9 @@
+package com.adverolt.app_api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fotos")
+public class FotoController {
+}
