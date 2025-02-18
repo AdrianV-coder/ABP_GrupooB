206bef262357ef4d33c57d1dbc69c061ca19671c
Adrián Vernich <vernicholtraadrian@gmail.com>
2025-02-18 13:04:53 +0100

    App C++ finalizada

--- Desenvolupament d'interfícies/App_Escritorio/moc_dtablausuarios.cpp
+++ Desenvolupament d'interfícies/App_Escritorio/moc_dtablausuarios.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'dtablausuarios.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.8)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.13)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -13,7 +13,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'dtablausuarios.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.15.8. It"
+#error "This file was generated using the moc from 5.15.13. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_DTablaUsuarios_t {
-    QByteArrayData data[1];
-    char stringdata0[15];
+    QByteArrayData data[7];
+    char stringdata0[98];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,10 +32,18 @@ struct qt_meta_stringdata_DTablaUsuarios_t {
     )
 static const qt_meta_stringdata_DTablaUsuarios_t qt_meta_stringdata_DTablaUsuarios = {
     {
-QT_MOC_LITERAL(0, 0, 14) // "DTablaUsuarios"
+QT_MOC_LITERAL(0, 0, 14), // "DTablaUsuarios"
+QT_MOC_LITERAL(1, 15, 19), // "slotEliminarUsuario"
+QT_MOC_LITERAL(2, 35, 0), // ""
+QT_MOC_LITERAL(3, 36, 17), // "slotElegirUsuario"
+QT_MOC_LITERAL(4, 54, 18), // "slotAnyadirUsuario"
+QT_MOC_LITERAL(5, 73, 10), // "slotInicio"
+QT_MOC_LITERAL(6, 84, 13) // "procesarDatos"
 
     },
-    "DTablaUsuarios"
+    "DTablaUsuarios\0slotEliminarUsuario\0\0"
+    "slotElegirUsuario\0slotAnyadirUsuario\0"
+    "slotInicio\0procesarDatos"
 };
 #undef QT_MOC_LITERAL
 
@@ -45,22 +53,44 @@ static const uint qt_meta_data_DTablaUsuarios[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   39,    2, 0x0a /* Public */,
+       3,    0,   40,    2, 0x0a /* Public */,
+       4,    0,   41,    2, 0x0a /* Public */,
+       5,    0,   42,    2, 0x0a /* Public */,
+       6,    1,   43,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QByteArray,    2,
+
        0        // eod
 };
 
 void DTablaUsuarios::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    (void)_o;
-    (void)_id;
-    (void)_c;
-    (void)_a;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<DTablaUsuarios *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->slotEliminarUsuario(); break;
+        case 1: _t->slotElegirUsuario(); break;
+        case 2: _t->slotAnyadirUsuario(); break;
+        case 3: _t->slotInicio(); break;
+        case 4: _t->procesarDatos((*reinterpret_cast< QByteArray(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
 }
 
 QT_INIT_METAOBJECT const QMetaObject DTablaUsuarios::staticMetaObject = { {
@@ -91,11 +121,22 @@ void *DTablaUsuarios::qt_metacast(const char *_clname)
 int DTablaUsuarios::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 5)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 5;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 5)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 5;
+    }
     return _id;
 }
 struct qt_meta_stringdata_ModeloTabla_t {
-    QByteArrayData data[3];
-    char stringdata0[30];
+    QByteArrayData data[1];
+    char stringdata0[12];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -104,12 +145,10 @@ struct qt_meta_stringdata_ModeloTabla_t {
     )
 static const qt_meta_stringdata_ModeloTabla_t qt_meta_stringdata_ModeloTabla = {
     {
-QT_MOC_LITERAL(0, 0, 11), // "ModeloTabla"
-QT_MOC_LITERAL(1, 12, 16), // "slotTemporizador"
-QT_MOC_LITERAL(2, 29, 0) // ""
+QT_MOC_LITERAL(0, 0, 11) // "ModeloTabla"
 
     },
-    "ModeloTabla\0slotTemporizador\0"
+    "ModeloTabla"
 };
 #undef QT_MOC_LITERAL
 
@@ -119,32 +158,21 @@ static const uint qt_meta_data_ModeloTabla[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   14, // methods
+       0,    0, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
- // slots: name, argc, parameters, tag, flags
-       1,    0,   19,    2, 0x0a /* Public */,
-
- // slots: parameters
-    QMetaType::Void,
-
        0        // eod
 };
 
 void ModeloTabla::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    if (_c == QMetaObject::InvokeMetaMethod) {
-        auto *_t = static_cast<ModeloTabla *>(_o);
-        (void)_t;
-        switch (_id) {
-        case 0: _t->slotTemporizador(); break;
-        default: ;
-        }
-    }
+    (void)_o;
+    (void)_id;
+    (void)_c;
     (void)_a;
 }
 
@@ -174,17 +202,6 @@ void *ModeloTabla::qt_metacast(const char *_clname)
 int ModeloTabla::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QAbstractTableModel::qt_metacall(_c, _id, _a);
-    if (_id < 0)
-        return _id;
-    if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 1)
-            qt_static_metacall(this, _c, _id, _a);
-        _id -= 1;
-    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 1)
-            *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 1;
-    }
     return _id;
 }
 QT_WARNING_POP
